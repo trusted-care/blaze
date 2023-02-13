794c7221b6d3937375934b5396d317cb127520e3
sebastianspiller <spillersebastian@googlemail.com>
2023-02-13 16:40:20 +0100

    add reactive-var, reactive-dict

---
+++ packages/reactive-dict/package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  summary: "Reactive dictionary",
+  version: '1.3.1'
+});
+
+Package.onUse(function (api) {
+  api.use(['tracker', 'ejson', 'ecmascript']);
+  // If we are loading mongo-livedata, let you store ObjectIDs in it.
+  api.use(['mongo', 'reload'], { weak: true });
+  api.mainModule('migration.js');
+  api.export('ReactiveDict');
+  api.addAssets('reactive-dict.d.ts', 'server');
+});
+
+Package.onTest(function (api) {
+  api.use('tinytest');
+  api.use('reactive-dict');
+  api.use('tracker');
+  api.use('reload');
+  api.addFiles('reactive-dict-tests.js');
+});
